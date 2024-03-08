693f118565ca3dbee4687d5c7442491890da94e2
alexandrapesantez <103144953+alexandrapesantez@users.noreply.github.com>
2024-03-08 16:15:42 -0700

    finished product

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import teamsData from './CollegeBasketballTeams.json';
+import React, { ReactNode } from 'react';
+
+interface Team {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function TeamCard({ team }: { team: Team }) {
+  const { school, name, city, state } = team;
+
+  return (
+    <div className="card">
+      <h2>{school}</h2>
+      <p>{name}</p>
+      <p>
+        {city}, {state}
+      </p>
+    </div>
+  );
+}
+
+function WelcomeHeader() {
+  return (
+    <div>
+      <h1>Welcome to NCAA Teams List</h1>
+      <p>
+        This page details every college basketball team competing in March
+        Madness!
+      </p>
+    </div>
+  );
+}
+
+function CollegeList({ teams }: { teams: Team[] }) {
+  return (
+    <div>
+      {teams.map((team, index) => (
+        <TeamCard key={team.school} team={team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WelcomeHeader />
+      <CollegeList teams={teamsData.teams} />
     </div>
   );
 }
